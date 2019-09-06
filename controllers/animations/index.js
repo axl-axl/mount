319c649351ae91c8e5deda1a9e23cb6ee2bc798d
zuoke <ke.zuo@56qq.com>
2019-09-06 09:55:42 +0800

    feat: 添加pm2

--- controllers/animations/index.js
+++ controllers/animations/index.js
@@ -1,17 +1,48 @@
 'use strict';
+const { addAnimation, delAnimation, getAnimation, updateAnimation } = require('../../services/animationService');
+module.exports = function (fastify, opts, next) {
+  fastify.get('/example', function (request, reply) {
+    reply.send('this is')
+  })
+  fastify.post('/addAnimation', {
+    schema: {
+      body: {
+        type: 'object',
+      }
+    }
+  }, function (request, reply) {
+    request.multipart(() => undefined, onFinish);
+    async function onFinish(error) {
+      if (error) {
+        // 
+      }
+      const result = addAnimation(fastify, request.body);
+      result.then(res => reply.send(res), error => reply.send(error))
+    }
+  });
 
-// module.exports = function (fastify, opts, next) {
-//   fastify.get('/example', function (request, reply) {
-//     reply.send('this is')
-//   })
+  fastify.post('/delAnimation', function(request, reply) {
+    const { id } = request.body;
+    const result = delAnimation(fastify, id)
+    result.then(res => reply.send(res), error => reply.send(error))
+  });
 
-//   next()
-// }
+  fastify.get('/getAnimation', function(request, reply) {
+    const result = getAnimation(fastify, request.query)
+    result.then(res => reply.send(res[0]), error => reply.send(error))
+  });
+
+  fastify.post('/updateAnimation', function(request, reply) {
+    const { id, ...datas } = request.body;
+    const result = updateAnimation(fastify, id, datas);
+    result.then(res => reply.send(res[0]), error => reply.send(error))
+  })
+  next()
+}
 
 // If you prefer async/await, use the following
-const { addAnimation, delAnimation, getAnimation, updateAnimation } = require('../../services/animationService');
 
-module.exports = async function (fastify) {
+// module.exports = async function (fastify) {
   // fastify.get('/example', {
   //   onRequest: function (request, replay, done) {
   //     done()
@@ -35,39 +66,4 @@ module.exports = async function (fastify) {
   // async function (request, reply) {
   //   reply.send(request.params);
   // });
-
-  fastify.post('/addAnimation', {
-    schema: {
-      body: {
-        type: 'object',
-      }
-    }
-  }, async function (request, reply) {
-    request.multipart(() => undefined, onFinish);
-    async function onFinish(error) {
-      if (error) {
-        // 
-      }
-      const result = addAnimation(fastify, request.body);
-      result.then(res => reply.send(res), error => reply.send(error))
-    }
-  });
-
-  fastify.post('/delAnimation', async function(request, reply) {
-    const { id } = request.body;
-    const result = delAnimation(fastify, id)
-    result.then(res => reply.send(res), error => reply.send(error))
-  });
-
-  fastify.get('/getAnimation', async function(request, reply) {
-    const result = getAnimation(fastify, request.query)
-    result.then(res => reply.send(res[0]), error => reply.send(error))
-  });
-
-  fastify.post('/updateAnimation', async function(request, reply) {
-    const { id, ...datas } = request.body;
-    console.log(id, datas);
-    const result = updateAnimation(fastify, id, datas);
-    result.then(res => reply.send(res[0]), error => reply.send(error))
-  })
-}
+// }
